ffcfebe187c481bb7dc106e7cfc771b067ce7b05
JackZhang <1021938637@qq.com>
2018-01-22 20:34:37 -0800

    multiple platform token

--- src/main/java/org/jon/lv/common/TokenContants.java
+++ src/main/java/org/jon/lv/common/TokenContants.java
@@ -7,8 +7,19 @@ import java.util.concurrent.TimeUnit;
  */
 public class TokenContants {
 
+    /**
+     * 过期时间
+     */
     public static final long TOKEN_EXPIRES_TIME = 100;
+    /**
+     * 过期时间单位
+     */
     public static final TimeUnit TOKEN_EXPIRES_TIMEUNIT = TimeUnit.SECONDS;
+
+    /**
+     * 是否允许多平台登录
+     */
+    public static final boolean ENABLE_MULTI_CLIENT = true;
 
 
 }
